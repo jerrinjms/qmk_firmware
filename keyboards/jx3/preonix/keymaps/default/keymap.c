e9e6b7252ec3935b35bc93f31f992e510f939e45
jerrinjms <jerrinjms@gmail.com>
2023-09-24 00:25:50 -0400

    minor updates to keymap.c
    formatting the keymap.c file

--- keyboards/jx3/preonix/keymaps/default/keymap.c
+++ keyboards/jx3/preonix/keymaps/default/keymap.c
@@ -25,17 +25,42 @@ enum layers {
 
 
  const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-    DEFAULT
 
+    /* Qwerty
+    * ,------------------------------------------------------------------------------------------------.
+    * |   1  |   2  |   3  |   4  |   5  |   6  |            |   7  |   8  |   9  |   0  |  -/_ |  =/+ |
+    * |------+------+------+------+------+------+-------------------+------+------+------+------+------|
+    * | Tab  |   Q  |   W  |   E  |   R  |   T  |            |   Y  |   U  |   I  |   O  |   P  | Bksp |
+    * |------+------+------+------+------+--------------------------+------+------+------+------+------|
+    * | Esc  |   A  |   S  |   D  |   F  |   G  |            |   H  |   J  |   K  |   L  |   ;  |  "   |
+    * |------+------+------+------+------+------|-------------------+------+------+------+------+------|
+    * | Shift|   Z  |   X  |   C  |   V  |   B  |            |   N  |   M  |   ,  |   .  |   /  | Enter|
+    * |------+------+------+------+------+------+-------------------+------+------+------+------+------|
+    * | Ctrl | GUI  |  Alt | CapL |Lower |Space |            |Space |Raise | Left | Down |  UP  |Right |
+    * `------------------------------------------------------------------------------------------------'
     */
+
     [_DEFAULT] = LAYOUT_ortho_5x12(
-        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-        QK_GESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, SC_SENT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_CAPS, TT(1), KC_SPC, KC_SPC, TT(2), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+        KC_1,     KC_2,     KC_3,     KC_4,     KC_5,   KC_6,    KC_7,    KC_8,   KC_9,     KC_0,     KC_MINS, KC_EQL,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,    KC_Y,    KC_U,   KC_I,     KC_O,     KC_P,    KC_BSPC,
+        QK_GESC,  KC_A,     KC_S,     KC_D,     KC_F,   KC_G,    KC_H,    KC_J,   KC_K,     KC_L,     KC_SCLN, KC_QUOT,
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,    KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH, SC_SENT,
+        KC_LCTL,  KC_LGUI,  KC_LALT,  KC_CAPS,  TT(1),  KC_SPC,  KC_SPC,  TT(2),  KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT
     ),
+
+    /* Lower
+    * ,------------------------------------------------------------------------------------------------.
+    * |   -  |   -  |   -  |   -  |   -  |   -  |            |   -  |   8  |   9  |   0  |   -  | PtSn |
+    * |------+------+------+------+------+------+-------------------+------+------+------+------+------|
+    * |   -  |   Q  |   W  |   E  |   R  |   T  |            |   Y  |   U  |   I  |   O  |   P  | Bksp |
+    * |------+------+------+------+------+--------------------------+------+------+------+------+------|
+    * |   -  |   A  |   S  |   D  |   F  |   G  |            |   H  |   J  |   K  |   L  |   ;  |  "   |
+    * |------+------+------+------+------+------|-------------------+------+------+------+------+------|
+    * | Shift|   Z  |   X  |   C  |   V  |   B  |            |   N  |   M  |   ,  |   .  |   /  | Enter|
+    * |------+------+------+------+------+------+-------------------+------+------+------+------+------|
+    * | Ctrl |   -  |  Alt | CapL |Lower |Space |            |Space |Raise | Left | Down |  UP  |Right |
+    * `------------------------------------------------------------------------------------------------'
+    */
 
     [_LOWER] = LAYOUT_ortho_5x12(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NUM, KC_PSLS, KC_PAST, KC_PMNS, KC_NO, KC_PSCR,
